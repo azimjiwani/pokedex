29695f2d34039c73daf2a313de2228c29610a0d1
Azim Jiwani <azimjiwani@hotmail.com>
2022-12-24 15:28:24 -0500

    Initial project setup (#1)

---
+++ pokedex/pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Azim Jiwani on 2022-12-24.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
