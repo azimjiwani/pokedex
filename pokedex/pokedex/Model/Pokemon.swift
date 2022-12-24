216e44d8c9f89fb06d3c3b1bb46d4bee7721c5b0
Azim Jiwani <azimjiwani@hotmail.com>
2022-12-24 15:36:39 -0500

    Create data models (#2)

---
+++ pokedex/pokedex/Model/Pokemon.swift
@@ -0,0 +1,19 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Azim Jiwani on 2022-12-24.
+//
+
+import Foundation
+
+struct Pokemon: Decodable {
+    let name: String
+    let sprites: Sprites
+    
+    var imageData: Data {
+        guard let url = URL(string: sprites.frontDefault) else { return Data() }
+        guard let data = try? Data(contentsOf: url) else { return Data() }
+        return data
+    }
+}
