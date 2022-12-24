3acb0a6a5a8bd378e7d3f3ad5e1bb1e39dad2806
Azim Jiwani <azimjiwani@hotmail.com>
2022-12-24 15:49:10 -0500

    Create viewmodel (#6)

---
+++ pokedex/pokedex/ViewModel/PokemonListViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  PokemonListViewModel.swift
+//  pokedex
+//
+//  Created by Azim Jiwani on 2022-12-24.
+//
+
+import Foundation
+import Combine
+
+class PokemonListViewModel: ObservableObject {
+  @Published var pokemons: [Pokemon] = []
+
+  private let pokeAPI: PokeAPI
+
+  init(pokeAPI: PokeAPI) {
+    self.pokeAPI = pokeAPI
+  }
+
+  func fetchPokemon() {
+    pokeAPI.fetchPokemon { [weak self] result in
+      guard let self = self else { return }
+      switch result {
+      case .success(let pokemons):
+        self.pokemons = pokemons
+      case .failure(let error):
+        print(error)
+      }
+    }
+  }
+}
