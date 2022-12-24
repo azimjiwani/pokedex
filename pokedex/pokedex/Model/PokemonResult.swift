0184f5943063942d4139f30bd0cb3de624840db7
Azim Jiwani <azimjiwani@hotmail.com>
2022-12-24 15:36:12 -0500

    Create data models

---
+++ pokedex/pokedex/Model/PokemonResult.swift
@@ -0,0 +1,11 @@
+//
+//  PokemonResult.swift
+//  pokedex
+//
+//  Created by Azim Jiwani on 2022-12-24.
+//
+
+import Foundation
+struct PokemonResult: Decodable {
+  let results: [Pokemon]
+}
---
+++ pokedex/pokedex/Model/Sprites.swift
@@ -0,0 +1,12 @@
+//
+//  Sprites.swift
+//  pokedex
+//
+//  Created by Azim Jiwani on 2022-12-24.
+//
+
+import Foundation
+
+struct Sprites: Decodable {
+  let frontDefault: String
+}
